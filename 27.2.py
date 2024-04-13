d4331a7bd6087bb1041d34b7029e30dc99efe2de
bunturioto <Bunturioto@gmail.com>
2024-04-13 13:24:53 +0400

    print all tasks are completed, run the main asynchronous function

--- 27.2.py
+++ 27.2.py
@@ -6,9 +6,13 @@ import random
 async def main():
     # Iterate over numbers from 1 to 10
     for i in range(1, 11):
-        # Generate a random delay between 1 and 5 seconds
+        # Generate a random delay
         delay = random.randint(1, 5)
-        # Pause the execution of the coroutine for the randomly chosen delay
         await asyncio.sleep(delay)
         # Print the current number and the delay
         print(f"{i}, delay: {delay}")
+    # print all tasks are completed
+    print("All Tasks Completed")
+
+# run the main asynchronous function
+asyncio.run(main())
