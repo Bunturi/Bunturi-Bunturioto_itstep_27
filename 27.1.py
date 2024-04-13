a8d8e7517ad52c998c79c37dd545a811f94c861f
bunturioto <Bunturioto@gmail.com>
2024-04-13 13:17:53 +0400

    Define the main asynchronous function, Gather both asynchronous tasks to execute concurrently

--- 27.1.py
+++ 27.1.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 # Define an asynchronous function to simulate drinking coffee
 async def drink_coffee():
@@ -11,3 +12,18 @@ async def eat_breakfast():
     print("Start Eating Breakfast")
     await asyncio.sleep(5)
     print("Finished Eating Breakfast")
+
+
+# Define the main asynchronous function
+async def main():
+    start_time = time.time()  # Record the start time
+
+    # Gather both asynchronous tasks to execute concurrently
+    all_tasks = asyncio.gather(drink_coffee(), eat_breakfast())
+    await all_tasks
+
+    end_time = time.time()  # Record the end time
+    elapsed_time = end_time - start_time  # Calculate the elapsed time
+    print(f"Elapsed Time: {elapsed_time:.2f} Seconds")
+
+
